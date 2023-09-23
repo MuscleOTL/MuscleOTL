b7445f431183557dbcaea1e925d5dacbf91f739e
jeffnzk <jchan7240@gmail.com>
2023-09-23 15:49:17 -0400

    fixed server endpoint/router

--- server/server.js
+++ server/server.js
@@ -11,22 +11,28 @@ app.use('api/bundle', express.static(path.join(__dirname, '../bundle')));
 app.use(express.json());
 app.use(bodyParser.json());
 
-// router 
-const dbRouter = express.Router();
 
-dbRouter.get('api/:muscle', sqlController.getExercises, (req, res, next) => {
+// router
+
+// const dbRouter = express.Router();
+// dbRouter.get('/api/:muscle', sqlController.getExercises, (req, res, next) => {
+//     return res.status(200).json(res.locals.exerciseResult);
+// });
+// app.use('/api', dbRouter);
+
+// get all exercises of a given muscle (ID)
+app.get('/api/:muscle', sqlController.getExercises, (req, res, next) => {
     return res.status(200).json(res.locals.exerciseResult);
 });
 
 
 
 //404 if page doesn't exist
-app.use('*', (err, req, res) => {
+app.use('*', (err, req, res, next) => {
     const error = new Error(`Can't find ${res.originalUrl} on the server`);
     err.status = 'fail';
     err.statusCode = 404;
-    
-    next(err);
+    return next(err);
 });
 
 //Global error handler
@@ -36,8 +42,7 @@ app.use((err, req, res, next) => {
     res.status(err.statusCode).json({   //internal server error
         status: err.statusCode,
         message: err.message
-    }); 
-
+    });
 })
 
 
