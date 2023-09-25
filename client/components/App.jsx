4e7ac518cdca4e5e0769ae308c36d9b05c98af31
AArCruz <142547463+AArCruz@users.noreply.github.com>
2023-09-25 09:57:24 -0400

    monday commit, no changes made

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import Home from './bodyComponent';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from './homeComponent';
 
-function App() {}
-
-//helloooo
+function App() {
+    
+    return (
+        <>
+        <BrowserRouter>
+        <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/body" element={<Body/>}/>
+        </Routes>
+        </BrowserRouter>
+        </>
+    );
+}
 
 export default App;
